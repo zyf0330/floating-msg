045821da4c993a4c6c9384fc20a0db67ed56a5fe
zhongfan <fan.zhong@duobei.com>
2019-10-02 00:34:56 +0800

    改变逻辑

--- client.js
+++ client.js
@@ -2,15 +2,10 @@ const io = require('socket.io-client')
 const client = io('http://localhost:3000')
 
 client.on('text', (text) => {
-  console.log('text', text)
+//  console.log('text', text)
 })
-
-client.on('initTexts', (texts) => {
-  console.log('initTexts', texts)
-})
-
 
 setInterval(() => {
   console.log('send text')
   client.emit('text', Date.now())
-}, 1000)
+}, 50)
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const io = require('socket.io')(3000)
+const fs = require('fs')
 
+const toSendSizePerSecond = +(process.argv[2] || '5')
 const clients = {}
-const texts = []
+const newTexts = []
+const sentTexts = []
 
 io.on('connection', (socket) => {
   const socketId = socket.id
@@ -15,17 +18,56 @@ io.on('connection', (socket) => {
   })
 
   socket.on('text', (text) => {
+    newText(text)
+  })
+})
+
+function newText(text) {
     if(typeof text != 'string') {
       text = text + ''
     }
-    console.log('text', text)
 
-    texts.push(text)
-    if(texts.length > 5) {
-      texts.shift()
-    }
+    console.log('newText', text)
+    newTexts.push(text)
+
+	fs.writeFile('./texts.txt', 
+	  `${new Date().toJSON()} ${text}\n`,
+	  {flag: 'a'}, (err) => {
+	  err && console.log(err)
+	})
+}
+
+// send text to clients
+const toSendSize = toSendSizePerSecond
+setInterval(() => {
+  const toSend = newTexts.splice(0, toSendSize)
+
+  // 无新弹幕减少发送数量
+  let _remainLen = toSendSize - toSend.length
+  if(_remainLen > toSendSize / 2) { 
+    _remainLen = Math.floor(_remainLen / 2)
+  }
+
+  // 随机拣取发送过的消息
+  const remainLen = _remainLen
+  if(remainLen > 0 && sentTexts.length > 0) {
+    const set = new Set()
+	while (set.size < remainLen){
+	  const previousSize = set.size
+	  const pickLen = Math.min(100, sentTexts.length)
+	  set.add(sentTexts.slice(0, pickLen)[Math.floor(pickLen * Math.random())])
+	  if(previousSize == set.size) { break }
+	}
+    toSend.push(...set)
+  }
+
+  sentTexts.push(...(toSend.slice(0, toSendSize - remainLen)))
+
+  toSend.forEach((text) => {
     io.sockets.emit('text', text)
   })
+}, 1000)
 
-  socket.emit('initTexts', texts)
+process.on('uncaughtException', (err) => {
+  console.log('uncaughtException', err)
 })
